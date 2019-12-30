46446d6404a3e7d0664038ac0b4bfb41f30eb7a0
LE TRONG TINH <letrongtinhtdnu97@gmail.com>
2019-12-30 14:58:14 +0700

    Update

---
+++ setup-antd/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+import Dashboard from './containers/layout/Dashboard/index'
+
+function App() {
+  return (
+    <Dashboard />
+  );
+}
+
+export default App;
